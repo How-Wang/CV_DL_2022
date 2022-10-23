e1d559ae472ebcbbdad89838012cff2d528d8375
How-Wang <howard.h.wang.23@gmail.com>
2022-10-24 00:12:41 +0800

    Draft down

--- HW1_2/Img_Classifier/training.py
+++ HW1_2/Img_Classifier/training.py
@@ -1,6 +1,12 @@
 import torchvision.datasets as datasets
 import torch
+import torch.nn as nn
+import torchvision
 from torchvision import transforms
+
+import gc
+gc.collect()
+torch.cuda.empty_cache()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -11,9 +17,7 @@ learning_rate = 0.001
 transform = transforms.Compose([
     transforms.Resize(size=(224, 224)),
     transforms.ToTensor(),
-    # transforms.Normalize( 
-    #    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010) 
-    # )
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
@@ -21,3 +25,53 @@ cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, tran
 
 cor_class_list = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
+train_loader = torch.utils.data.DataLoader(cifar_trainset
+    , batch_size = batch_size
+    , shuffle = True)
+test_loader = torch.utils.data.DataLoader(cifar_testset
+    , batch_size = batch_size
+    , shuffle = True)
+
+n_total_step = len(train_loader)
+
+print(n_total_step)
+
+model = torchvision.models.vgg19(pretrained = True)
+input_lastLayer = model.classifier[6].in_features # last layer input num
+model.classifier[6] = nn.Linear(input_lastLayer,10) # change last layer output
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), 
+                        lr = learning_rate, 
+                        momentum=0.9,
+                        weight_decay=5e-4)
+
+for epoch in range(num_epochs):
+    for i, (imgs , labels) in enumerate(train_loader):
+        imgs = imgs.to(device)
+        labels = labels.to(device)
+
+        labels_hat = model(imgs)
+        n_corrects = (labels_hat.argmax(axis=1)==labels).sum().item()
+        loss_value = criterion(labels_hat, labels)
+        loss_value.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if (i+1) % 250 == 0:
+            print(f'epoch {epoch+1}/{num_epochs}, step: {i+1}/{n_total_step}: loss = {loss_value:.5f}, acc = {100*(n_corrects/labels.size(0)):.2f}%')
+    print()
+
+
+with torch.no_grad():
+    number_corrects = 0
+    number_samples = 0
+    for i, (test_images_set , test_labels_set) in enumerate(test_loader):
+        test_images_set = test_images_set.to(device)
+        test_labels_set = test_labels_set.to(device)
+    
+        y_predicted = model(test_images_set)
+        labels_predicted = y_predicted.argmax(axis = 1)
+        number_corrects += (labels_predicted==test_labels_set).sum().item()
+        number_samples += test_labels_set.size(0)
+    print(f'Overall accuracy {(number_corrects / number_samples)*100}%')
